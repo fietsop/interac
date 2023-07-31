f4f408d06678e0358fe0bfe507c73bfcb2131913
alain foryim <alinocooll.af@gmail>
2023-07-31 12:09:17 -0500

    vpc created

--- main.tf
+++ main.tf
@@ -1,3 +1,14 @@
 provider "aws" {
   
 }
+
+resource "aws_vpc" "depop" {
+    cidr_block = "10.0.0.0/16"
+    enable_dns_hostnames = true
+    enable_dns_support = true
+
+    tags = {
+      "Name" = "dev"
+    }
+  
+}
