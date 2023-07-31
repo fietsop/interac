332cab83e3e26252fe282b835cc28c6e402a91f5
alain foryim <alinocooll.af@gmail>
2023-07-31 12:26:02 -0500

    prt and pub subnets created

--- main.tf
+++ main.tf
@@ -12,3 +12,43 @@ resource "aws_vpc" "depop" {
     }
   
 }
+resource "aws_subnet" "dev01pubsub1" {
+    cidr_block = "10.0.1.0/24"
+    vpc_id = aws_vpc.depop.id
+
+    tags = {
+      "Name" = "devpubsubnet1" 
+    }
+
+  
+}
+resource "aws_subnet" "dev01pubsub2" {
+    cidr_block = "10.0.2.0/24"
+    vpc_id = aws_vpc.depop.id
+
+    tags = {
+      "Name" = "devpubsubnet2" 
+    }
+
+  
+}
+resource "aws_subnet" "dev01prtsub1" {
+    cidr_block = "10.0.3.0/24"
+    vpc_id = aws_vpc.depop.id
+
+    tags = {
+      "Name" = "devprtsubnet1" 
+    }
+
+  
+}
+resource "aws_subnet" "dev01prtsub2" {
+    cidr_block = "10.0.4.0/24"
+    vpc_id = aws_vpc.depop.id
+
+    tags = {
+      "Name" = "devprtsubnet2" 
+    }
+
+  
+}
