5fadaf46b5b985e696471841240985aac0947f70
alain foryim <alinocooll.af@gmail>
2023-08-04 18:50:37 -0500

    subnets created

--- main.tf
+++ main.tf
@@ -3,4 +3,23 @@ provider "aws" {
 }
 resource "aws_vpc" "main" {
   cidr_block = "10.0.0.0/16"
+  tags = {
+    "Name" = "Dev01" 
+  }
+}
+resource "aws_subnet" "main-pubsubnet" {
+  vpc_id = aws_vpc.main.id
+  cidr_block = "10.0.1.0/24"
+  tags = {
+    "Name" = "Dev01-pubsubnet" 
+  }
+
+}
+resource "aws_subnet" "main-privatesubnet" {
+  vpc_id = aws_vpc.main.id
+  cidr_block = "10.0.2.0/24"
+  tags = {
+    "Name" = "Dev01-privatesubnet" 
+  }
+
 }
