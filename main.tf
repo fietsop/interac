b5efcf725902137cde99721452c68249740b7714
alain foryim <alinocooll.af@gmail>
2023-08-05 08:55:26 -0500

    error fixed

--- main.tf
+++ main.tf
@@ -80,7 +80,7 @@ resource "aws_security_group" "ALB-lb_sg" {
     from_port        = 80
     to_port          = 80
     protocol         = "tcp"
-    cidr_blocks      = [0.0.0.0/0]
+    cidr_blocks      = ["0.0.0.0/0"]
     
 }
 ingress {
@@ -88,7 +88,7 @@ ingress {
     from_port        = 443
     to_port          = 443
     protocol         = "tcp"
-    cidr_blocks      = [0.0.0.0/0]
+    cidr_blocks      = ["0.0.0.0/0"]
     
 }
 egress {
